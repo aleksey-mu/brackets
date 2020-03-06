23918ba131caa9bf0ef0fdfea2d86d1bc11d59a6
aleksey_mu <pacuklex@mail.ru>
2020-03-07 00:10:50 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  module.exports = function check(str, bracketsConfig) {
+    const bracketsPattern = [];
+    const maxCount = str.length / 2;
+    bracketsConfig.forEach(element => {
+      bracketsPattern.push(element.join(''));
+    });
+
+    let count = 0; 
+
+    while (count < maxCount) {
+      for (pattern of bracketsPattern) {
+        str = str.replace(pattern, '');
+      }
+      count += 1;
+    }
+
+    return str.length ? false : true;
+  }
